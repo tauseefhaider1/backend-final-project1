62247a05701a42e24faf878a4d805bd319301986
tauseefhaider1 <tauseefhaider119@gmail.com>
2026-01-13 12:20:18 +0500

    Use process.env.PORT for cloud deployment

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:3000",
+      "https://clientfinalproject.vercel.app",  // ✅ add this
     ],
     credentials: true,
   })
